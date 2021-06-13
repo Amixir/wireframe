7bf4fc94d84f93f1dfe7c02b3bd63fdec439a66e
Amixir <alejopq639@gmail.com>
2021-06-12 21:54:42 -0500

    pagina principal

---
+++ script.php
@@ -0,0 +1,35 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title></title>
+<meta <charset="utf-8">
+<link href="css/new.css" rel="stylesheet"/>
+<script src="js/main.js"></script>
+</head>
+<body>
+<div class="container">
+<div class="title">
+<main class="title">
+<h1> USUARIOS REGISTRADOS </h1>
+</main>	
+<div class="user-container">
+<div class="usuarios">
+<main class="contenedor">
+	<div class"contenedor-usuarios">
+	<h3 class="titulo"> Marcela Aguirre </h3>
+	<h3 class="titulo"> Alejandro Peña </h3>
+	<h3 class="titulo"> Kelly Jerrera </h3>
+	<h3 class="titulo"> Jeison Herrera </h3>
+	<h3 class="titulo"> Yuliana Peña </h3>
+	<?php
+//var_dump($_POST);
+echo $_POST['usuario']."<br>"."<br>";
+?>
+</div>
+	</main>
+
+<form action="index.html" >
+	<input type="submit" class="submit" value="salir"><br>
+	</form>
